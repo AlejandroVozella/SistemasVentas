d6913754d45a416683d87f1903b6c7283e7491a0
Alejandro Vozella <alejandrovozella@gmail.com>
2022-01-05 18:16:05 -0300

    Agregar archivos de proyecto.

---
+++ SistemasVentas/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MiLibreria;
+
+namespace SistemasVentas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Utilidades.Ejecutar("Select * FROM Clientes Where id =1");
+            
+        }
+    }
+}
